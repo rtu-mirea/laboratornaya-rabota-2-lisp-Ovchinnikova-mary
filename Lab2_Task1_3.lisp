2b1503d40ce98e9cdbb75d68367974843991e6f1
Ovchinnikova-mary <61615971+Ovchinnikova-mary@users.noreply.github.com>
2020-03-28 13:06:28 +0300

    Add files via upload

---
+++ Lab2_Task1_3.lisp
@@ -0,0 +1,18 @@
+(defun search (N list)
+   (cond ((eq list nil) "НЕ НАЙДЕН!")
+((eq (car list) N) "НАЙДЕН!")
+(t (search N (cdr list)))
+   )
+)
+(defun input ()
+    (print "Список:")
+    (print LIST)
+    (print "Введите искомый элемент") 
+    (set 'N '(read))
+    (print "Искомый элемент:") 
+    (search N LIST)
+   
+    
+)
+(set 'LIST '(76 7 13 5 32 3 667 1))
+(input)
---
+++ Lab2_Task2.lisp
@@ -0,0 +1,5 @@
+(let ((in (open "e:/text_Task2.txt" :if-does-not-exist nil)))
+               (when in
+                 (loop for line = (read-line in nil)
+                      while line do (format t "~a~%" line))
+                 (close in)))
